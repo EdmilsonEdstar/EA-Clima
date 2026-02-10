6177abc8335ad76e05452b031e5a0806ad75a113
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2026-02-10 16:25:26 +0000

    alteração da cor dos card

--- src/ShowWheatherWeek/ShowWheatherWeek.tsx
+++ src/ShowWheatherWeek/ShowWheatherWeek.tsx
@@ -1,10 +1,10 @@
-import { Sun } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 
 export function ShowWheatherWeek() {
 
   return (
         <>
-             <div className="w-full flex justify-center items-center overflow-y-auto text-center gap-[4rem] py-8 px-4  bg-gray-100/70 rounded-2xl ">
+             <div className="w-full flex justify-between items-center overflow-y-auto text-center gap-16 py-8 px-8  bg-gray-100/70 rounded-2xl ">
                 <div className="flex flex-col justify-center items-center gap-2">
                     <span className="text-gray-500">Seg</span>
                     <div className="flex justify-center items-center gap-2 text-xl">
@@ -16,7 +16,7 @@ export function ShowWheatherWeek() {
                 <div className="flex flex-col justify-center items-center gap-2">
                     <span className="text-gray-500">Ter</span>
                     <div className="flex justify-center items-center gap-2 text-xl">
-                      <Sun  size={20} fill="yellow" stroke="orange" /><span className="text-gray-500">30ºC</span> 
+                      <Moon  size={20} fill="#cacaec" stroke="#7a7aea" /><span className="text-gray-500">30ºC</span> 
                     </div>
                     <span className="text-gray-500">Céu Limpo</span>
                 </div>
