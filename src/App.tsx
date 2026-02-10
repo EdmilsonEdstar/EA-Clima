d80a99e0da2b31083eba60509a8a21c238ec94c6
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2026-02-10 13:01:08 +0000

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./index.css"
+
+function App() {
+
+  return (
+        <main className="w-">
+          <div>
+             <label htmlFor="search">Previsão do tempo</label>
+             <div>
+                <span></span>
+                <input type="text" id="search" placeholder="" className="" />
+             </div>
+             <input type="text" id="search" placeholder="" className="" />
+          </div>
+        </main>
+  )
+}
+
+export default App
